c71ca6a2fb6578cc8f478f8f200fdc10c4d3b29e
Vadim Konshin <vdmkonshin@gmail.com>
2024-02-08 16:54:02 +0200

    add new func (func_title)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(string):
+    """Функция делает все буквы заглавными"""
     return string.upper
+
+
+def func_title(string):
+    """Функция делает первые буквы заглавными"""
+    return string.tittle()
